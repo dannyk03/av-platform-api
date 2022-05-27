a1a3eaf329532bdfb6581ef94f32cec9db478732
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-05-28 00:11:48 +0300

    Add moduleNameMapper for src folder + ts fix for intellisense

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import Configs from 'src/config';
+import Configs from '@/config';
 
 @Module({
   controllers: [],
