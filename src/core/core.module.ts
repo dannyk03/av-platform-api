2cb5438e7583bf0d54ab123843ed520aaa660ff3
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-05-28 00:02:39 +0300

    Add CoreModule with ConfigModule, and configuration files

---
+++ src/core/core.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Configs from 'src/config';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: Configs,
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      ignoreEnvFile: false,
+      envFilePath: ['.env'],
+    }),
+  ],
+})
+export class CoreModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const env: string = configService.get<string>('app.env');
+  const host: string = configService.get<string>('app.http.host');
+  const port: number = configService.get<number>('app.http.port');
+
+  const logger = new Logger();
+  process.env.NODE_ENV = env;
+
+  await app.listen(port, host);
+
+  logger.log(`==========================================================`);
+  logger.log(`App Environment is ${env}`, 'NestApplication');
+  logger.log(
+    `App Language is ${configService.get<string>('app.language')}`,
+    'NestApplication',
+  );
+  logger.log(
+    `App Debug is ${configService.get<boolean>('app.debug')}`,
+    'NestApplication',
+  );
+
+  logger.log(
+    `App Task is ${configService.get<boolean>('app.taskOn')}`,
+    'NestApplication',
+  );
+  logger.log(
+    `Database Debug is ${configService.get<boolean>('database.debug')}`,
+    'NestApplication',
+  );
+  logger.log(`==========================================================`);
+  logger.log(
+    `Database running on ${configService.get<string>(
+      'database.host',
+    )}/${configService.get<string>('database.name')}`,
+    'NestApplication',
+  );
+  logger.log(`Server running on ${await app.getUrl()}`, 'NestApplication');
+  logger.log(`==========================================================`);
 }
 bootstrap();
