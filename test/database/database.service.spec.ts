8efbfe69984afc0022c922430029c261331a14ba
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-06-25 14:06:12 +0300

    Add basic test for helper Services, Config, DatabaseOptions, Debugger, Message (Services)

---
+++ test/database/database.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { CoreModule } from 'src/core/core.module';
+import { TypeOrmConfigService } from '@/database/service/typeorm-config.service';
+
+describe('DatabaseService', () => {
+  let databaseService: TypeOrmConfigService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CoreModule],
+    }).compile();
+
+    databaseService = moduleRef.get<TypeOrmConfigService>(TypeOrmConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(databaseService).toBeDefined();
+  });
+
+  describe('createMongooseOptions', () => {
+    it('should be called', async () => {
+      const test = jest.spyOn(databaseService, 'createTypeOrmOptions');
+
+      databaseService.createTypeOrmOptions();
+      expect(test).toHaveBeenCalled();
+    });
+
+    it('should be success', async () => {
+      const options = databaseService.createTypeOrmOptions();
+      jest
+        .spyOn(databaseService, 'createTypeOrmOptions')
+        .mockImplementation(() => options);
+
+      expect(databaseService.createTypeOrmOptions()).toBe(options);
+    });
+  });
+});
---
+++ test/debugger/debugger.options.service.spec.ts
@@ -0,0 +1,39 @@
+import { Test } from '@nestjs/testing';
+import { CoreModule } from '@/core/core.module';
+import { DebuggerOptionService } from '@/debugger/service/debugger.option.service';
+
+describe('DebuggerOptionService', () => {
+  let debuggerOptionService: DebuggerOptionService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CoreModule],
+    }).compile();
+
+    debuggerOptionService = moduleRef.get<DebuggerOptionService>(
+      DebuggerOptionService,
+    );
+  });
+
+  it('should be defined', () => {
+    expect(debuggerOptionService).toBeDefined();
+  });
+
+  describe('info', () => {
+    it('should be called', async () => {
+      const test = jest.spyOn(debuggerOptionService, 'createLogger');
+
+      debuggerOptionService.createLogger();
+      expect(test).toHaveBeenCalled();
+    });
+
+    it('should be success', async () => {
+      const options = debuggerOptionService.createLogger();
+      jest
+        .spyOn(debuggerOptionService, 'createLogger')
+        .mockImplementation(() => options);
+
+      expect(debuggerOptionService.createLogger()).toBe(options);
+    });
+  });
+});
