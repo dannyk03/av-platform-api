b777e05575aca71599a54c17e1fd67f8cd1fc2c1
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-07-04 11:34:45 +0300

    Fix aliase imports in config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import { ConfigModule } from '@nestjs/config';
-import AppConfig from '@/config/app.config';
-import AuthConfig from '@/config/auth.config';
-import DatabaseConfig from '@/config/database.config';
-import HelperConfig from '@/config/helper.config';
-import AwsConfig from '@/config/aws.config';
+import AppConfig from './app.config';
+import AuthConfig from './auth.config';
+import DatabaseConfig from './database.config';
+import HelperConfig from './helper.config';
+import AwsConfig from './aws.config';
 import UserConfig from './user.config';
 import FileConfig from './file.config';
 import MiddlewareConfig from './middleware.config';
