2d4652cb092656726e38eba7040d4f5cf5a24f84
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-07-25 18:52:15 +0300

    Add cloudinary integration test

---
+++ integration/cloudinary/cloudinary.constant.ts
@@ -0,0 +1,1 @@
+export const INTEGRATION_CLOUDINARY_URL = '/health/cloudinary';
--- integration/cloudinary/cloudinary.integration.spec.ts
+++ integration/cloudinary/cloudinary.integration.spec.ts
@@ -1,44 +1,45 @@
-// import { HttpStatus, INestApplication } from '@nestjs/common';
-// import { TerminusModule } from '@nestjs/terminus';
-// import { Test } from '@nestjs/testing';
-// import { CoreModule } from '@/core/core.module';
-// import { HealthController } from '@/health/controller/health.controller';
-// import { HealthModule } from 'src/health/health.module';
-// import { HelperDateService } from 'src/utils/helper/service/helper.date.service';
-// import request from 'supertest';
-// import { faker } from '@faker-js/faker';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import { Test } from '@nestjs/testing';
+import { CoreModule } from '@/core/core.module';
+import { HealthController } from '@/health/controller/health.controller';
+import { HealthModule } from 'src/health/health.module';
+import { HelperDateService } from 'src/utils/helper/service/helper.date.service';
+import request from 'supertest';
+import { faker } from '@faker-js/faker';
+import { INTEGRATION_CLOUDINARY_URL } from './cloudinary.constant';
 
-// describe('Database Integration', () => {
-//   let app: INestApplication;
-//   let helperDateService: HelperDateService;
-//   let timestamp: number;
+describe('Cloudinary Integration', () => {
+  let app: INestApplication;
+  let helperDateService: HelperDateService;
+  let timestamp: number;
 
-//   beforeEach(async () => {
-//     const moduleRef = await Test.createTestingModule({
-//       imports: [CoreModule, HealthModule, TerminusModule],
-//       controllers: [HealthController],
-//     }).compile();
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CoreModule, HealthModule, TerminusModule],
+      controllers: [HealthController],
+    }).compile();
 
-//     app = moduleRef.createNestApplication();
-//     helperDateService = app.get(HelperDateService);
+    app = moduleRef.createNestApplication();
+    helperDateService = app.get(HelperDateService);
 
-//     timestamp = helperDateService.timestamp();
-//     await app.init();
-//   });
+    timestamp = helperDateService.timestamp();
+    await app.init();
+  });
 
-//   it(`GET ${INTEGRATION_DATABASE_URL} Success`, async () => {
-//     const response = await request(app.getHttpServer())
-//       .get(INTEGRATION_DATABASE_URL)
-//       .set('user-agent', faker.internet.userAgent())
-//       .set('x-timestamp', timestamp.toString());
+  it(`GET ${INTEGRATION_CLOUDINARY_URL} Success`, async () => {
+    const response = await request(app.getHttpServer())
+      .get(INTEGRATION_CLOUDINARY_URL)
+      .set('user-agent', faker.internet.userAgent())
+      .set('x-timestamp', timestamp.toString());
 
-//     expect(response.status).toEqual(HttpStatus.OK);
-//     expect(response.body.statusCode).toEqual(HttpStatus.OK);
+    expect(response.status).toEqual(HttpStatus.OK);
+    expect(response.body.statusCode).toEqual(HttpStatus.OK);
 
-//     return;
-//   });
+    return;
+  });
 
-//   afterAll(async () => {
-//     await app.close();
-//   });
-// });
+  afterAll(async () => {
+    await app.close();
+  });
+});
