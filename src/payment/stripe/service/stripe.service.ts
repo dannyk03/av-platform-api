c97cc40778dca5091fef566e5d5d08bbf755f941
liatyo <liatyosef18@gmail.com>
2022-08-04 20:02:14 +0100

    Added stub for ceating an order

--- src/payment/stripe/service/stripe.service.ts
+++ src/payment/stripe/service/stripe.service.ts
@@ -40,4 +40,36 @@ export class StripeService {
     // ask stripe what is the tax for this product
     return 10;
   }
+
+  async createOrder(giftIntentId) {
+    // TODO: move to payment service
+    // load the GiftIntent object / it will be given as input
+
+    // build the Order and its OrderItems records
+    // calculate shipping, taxes
+
+    // save in the db
+
+    // return the Order and its OrderItems records as a result.
+    return {
+      id: '',
+      totalPrice: 80,
+      totalTax: 13,
+      totalShipping: 13,
+      orderItems: [
+        {
+          tax: 12,
+          shipping: 12,
+          totalPrice: 40,
+          basePrice: 16,
+        },
+        {
+          tax: 1,
+          shipping: 1,
+          totalPrice: 40,
+          basePrice: 38,
+        },
+      ],
+    };
+  }
 }
