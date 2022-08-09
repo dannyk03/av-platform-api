6cfe4406e3ff740f7dbb906b9d19434f2017db5e
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-08-09 13:01:25 +0300

    Fail test

--- test/helper/helper.string.service.spec.ts
+++ test/helper/helper.string.service.spec.ts
@@ -29,7 +29,7 @@ describe('HelperStringService', () => {
       const test = jest.spyOn(helperStringService, 'checkEmail');
 
       helperStringService.checkEmail('111');
-      expect(test).toHaveBeenCalledWith('111');
+      expect(test).toHaveBeenCalledWith('1');
     });
 
     it('should be success', async () => {
