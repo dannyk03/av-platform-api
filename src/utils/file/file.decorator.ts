373b77afac42c5b065f2ac7eb04eaeefcb887bb8
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-08-09 13:14:51 +0300

    SonarCloud temp fixes

--- src/utils/file/file.decorator.ts
+++ src/utils/file/file.decorator.ts
@@ -9,10 +9,11 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { IFileOptions } from './file.interface';
 
 import { EnumFileType } from './file.constant';
-import { FileAudioInterceptor } from './interceptor/file.audio.interceptor';
-import { FileExcelInterceptor } from './interceptor/file.excel.interceptor';
+// import { FileAudioInterceptor } from './interceptor/file.audio.interceptor';
+// import { FileExcelInterceptor } from './interceptor/file.excel.interceptor';
 import { FileImageInterceptor } from './interceptor/file.image.interceptor';
-import { FileVideoInterceptor } from './interceptor/file.video.interceptor';
+
+// import { FileVideoInterceptor } from './interceptor/file.video.interceptor';
 
 export const GetExtractFile = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
@@ -52,36 +53,42 @@ export function UploadFileSingle(field: string, options?: IFileOptions): any {
         }),
       ),
     );
-  } else if (options && options.type === EnumFileType.EXCEL) {
-    return applyDecorators(
-      UseInterceptors(
-        FileInterceptor(field),
-        FileExcelInterceptor({
-          required: options && options.required ? options.required : false,
-          extract: options && options.extract ? options.extract : false,
-          dto: options && options.dto ? options.dto : undefined,
-        }),
-      ),
-    );
-  } else if (options && options.type === EnumFileType.AUDIO) {
-    return applyDecorators(
-      UseInterceptors(
-        FileInterceptor(field),
-        FileAudioInterceptor({
-          required: options && options.required ? options.required : false,
-        }),
-      ),
-    );
-  } else if (options && options.type === EnumFileType.VIDEO) {
-    return applyDecorators(
-      UseInterceptors(
-        FileInterceptor(field),
-        FileVideoInterceptor({
-          required: options && options.required ? options.required : false,
-        }),
-      ),
-    );
   }
+
+  // if (options && options.type === EnumFileType.EXCEL) {
+  //   return applyDecorators(
+  //     UseInterceptors(
+  //       FileInterceptor(field),
+  //       FileExcelInterceptor({
+  //         required: options && options.required ? options.required : false,
+  //         extract: options && options.extract ? options.extract : false,
+  //         dto: options && options.dto ? options.dto : undefined,
+  //       }),
+  //     ),
+  //   );
+  // }
+
+  // if (options && options.type === EnumFileType.AUDIO) {
+  //   return applyDecorators(
+  //     UseInterceptors(
+  //       FileInterceptor(field),
+  //       FileAudioInterceptor({
+  //         required: options && options.required ? options.required : false,
+  //       }),
+  //     ),
+  //   );
+  // }
+
+  // if (options && options.type === EnumFileType.VIDEO) {
+  //   return applyDecorators(
+  //     UseInterceptors(
+  //       FileInterceptor(field),
+  //       FileVideoInterceptor({
+  //         required: options && options.required ? options.required : false,
+  //       }),
+  //     ),
+  //   );
+  // }
 
   return applyDecorators(UseInterceptors(FileInterceptor(field)));
 }
@@ -96,33 +103,39 @@ export function UploadFileMultiple(field: string, options?: IFileOptions): any {
         }),
       ),
     );
-  } else if (options && options.type === EnumFileType.EXCEL) {
+  }
+
+  if (options && options.type === EnumFileType.EXCEL) {
     return applyDecorators(
       UseInterceptors(
         FilesInterceptor(field),
-        FileExcelInterceptor({
-          required: options && options.required ? options.required : false,
-          extract: options && options.extract ? options.extract : false,
-          dto: options && options.dto ? options.dto : undefined,
-        }),
+        // FileExcelInterceptor({
+        //   required: options && options.required ? options.required : false,
+        //   extract: options && options.extract ? options.extract : false,
+        //   dto: options && options.dto ? options.dto : undefined,
+        // }),
       ),
     );
-  } else if (options && options.type === EnumFileType.AUDIO) {
+  }
+
+  if (options && options.type === EnumFileType.AUDIO) {
     return applyDecorators(
       UseInterceptors(
         FilesInterceptor(field),
-        FileAudioInterceptor({
-          required: options && options.required ? options.required : false,
-        }),
+        // FileAudioInterceptor({
+        //   required: options && options.required ? options.required : false,
+        // }),
       ),
     );
-  } else if (options && options.type === EnumFileType.VIDEO) {
+  }
+
+  if (options && options.type === EnumFileType.VIDEO) {
     return applyDecorators(
       UseInterceptors(
         FilesInterceptor(field),
-        FileVideoInterceptor({
-          required: options && options.required ? options.required : false,
-        }),
+        // FileVideoInterceptor({
+        //   required: options && options.required ? options.required : false,
+        // }),
       ),
     );
   }
