4a97253846692a2d3f4fca2817baaf34d4abd23e
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-08-11 13:26:33 +0300

    Fix pagination skip value

--- src/utils/pagination/service/pagination.service.ts
+++ src/utils/pagination/service/pagination.service.ts
@@ -6,7 +6,7 @@ import { PAGINATION_DEFAULT_MAX_PER_PAGE } from '../pagination.constant';
 export class PaginationService {
   async skip(page: number, perPage: number): Promise<number> {
     perPage = Math.min(PAGINATION_DEFAULT_MAX_PER_PAGE, perPage);
-    const skip: number = page * perPage;
+    const skip: number = (page - 1) * perPage;
 
     return skip;
   }
