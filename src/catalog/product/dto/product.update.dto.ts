eb46291b249631d18eb08aec29ef8fabf53840b8
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-08-14 16:50:14 +0300

    Cleaunp

--- src/catalog/product/dto/product.update.dto.ts
+++ src/catalog/product/dto/product.update.dto.ts
@@ -1,6 +1,5 @@
 import { EnumDisplayLanguage } from '@avo/type';
 
-import { Transform, Type } from 'class-transformer';
 import {
   IsArray,
   IsBoolean,
@@ -8,14 +7,10 @@ import {
   IsOptional,
   IsString,
   IsUUID,
-  Length,
   MaxLength,
 } from 'class-validator';
 
-import {
-  ProductDisplayLanguage,
-  ProductSKU,
-} from '@/catalog/catalog.decorator';
+import { ProductDisplayLanguage } from '@/catalog/catalog.decorator';
 
 import {
   ArrayTransform,
@@ -47,14 +42,6 @@ export class ProductUpdateDisplayDto {
 }
 
 export class ProductUpdateDto {
-  // @Length(3, 30)
-  // @ProductSKU()
-  // @IsOptional()
-  // @IsNotEmpty()
-  // @NormalizeStringInputTransform()
-  // @Type(() => String)
-  // readonly sku?: string;
-
   @IsOptional()
   @MaxLength(30)
   @NormalizeStringInputTransform()
