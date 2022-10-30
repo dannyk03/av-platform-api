ae10c6aeaa16d7f8ac538a3001f7a294a1609367
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-10-30 13:32:04 +0200

    Add GiftIntent and GiftOrder relation

--- src/order/entity/gift-order.entity.ts
+++ src/order/entity/gift-order.entity.ts
@@ -6,7 +6,11 @@ import { User } from '@/user/entity';
 
 @Entity()
 export class GiftOrder extends BaseEntity<GiftOrder> {
-  @OneToOne(() => GiftIntent, (giftIntent) => giftIntent.order)
+  @OneToOne(() => GiftIntent, (giftIntent) => giftIntent.order, {
+    onDelete: 'CASCADE',
+    orphanedRowAction: 'delete',
+  })
+  @JoinColumn()
   giftIntent!: GiftIntent;
 
   @ManyToOne(() => User, (user) => user.giftOrders, {
@@ -15,4 +19,10 @@ export class GiftOrder extends BaseEntity<GiftOrder> {
   })
   @JoinColumn()
   user!: User;
+
+  // All 'totalPrice' calculations should be performed in 'real time' against GiftIntent entity,
+  // and at the time of the desire to pay, because it is something dynamic and can change between the moment it was
+  // submitted and the moment the customer wants to pay.
+  // The goal in this dynamism is not to create duplicate data and stale data.
+  // This entity is more responsible for things like discounts or promo codes applied (in the future),
 }
