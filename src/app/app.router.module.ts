cd883bff81b5dcc4482695ef016b5c68e51dbf34
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-10-31 11:31:57 +0200

    Cleanup

--- src/app/app.router.module.ts
+++ src/app/app.router.module.ts
@@ -43,7 +43,6 @@ export class AppRouterModule {
           RouterHealthModule,
           RouterTestModule,
           RouterPaymentModule,
-          RouterPaymentModule,
           RouterModule.register(
             compact([
               {
