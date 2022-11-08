b2cf6290f50e3af5a4828da6d33bcdd80e2645d6
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-11-08 18:09:35 +0200

    Fix length change migrations

--- src/database/migrations/default/1667507004120-migration.ts
+++ src/database/migrations/default/1667507004120-migration.ts
@@ -5,33 +5,15 @@ export class migration1667507004120 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP INDEX "public"."idx_product_display_options_name"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "product_display_options" DROP COLUMN "name"
-        `);
-    await queryRunner.query(`
             ALTER TABLE "product_display_options"
-            ADD "name" character varying(75) NOT NULL
-        `);
-    await queryRunner.query(`
-            CREATE INDEX "idx_product_display_options_name" ON "product_display_options" ("name")
+            ALTER COLUMN "name" TYPE character varying(75)
         `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP INDEX "public"."idx_product_display_options_name"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "product_display_options" DROP COLUMN "name"
-        `);
-    await queryRunner.query(`
             ALTER TABLE "product_display_options"
-            ADD "name" character varying(50) NOT NULL
-        `);
-    await queryRunner.query(`
-            CREATE INDEX "idx_product_display_options_name" ON "product_display_options" ("name")
+            ALTER COLUMN "name" TYPE character varying(50)
         `);
   }
 }
--- src/database/migrations/default/1667907097946-migration.ts
+++ src/database/migrations/default/1667907097946-migration.ts
@@ -5,47 +5,15 @@ export class migration1667907097946 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP INDEX "public"."idx_products_sku"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products" DROP CONSTRAINT "uq_products_sku"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products" DROP COLUMN "sku"
-        `);
-    await queryRunner.query(`
             ALTER TABLE "products"
-            ADD "sku" character varying(40) NOT NULL
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products"
-            ADD CONSTRAINT "uq_products_sku" UNIQUE ("sku")
-        `);
-    await queryRunner.query(`
-            CREATE INDEX "idx_products_sku" ON "products" ("sku")
+            ALTER COLUMN "sku" TYPE character varying(40)
         `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            DROP INDEX "public"."idx_products_sku"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products" DROP CONSTRAINT "uq_products_sku"
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products" DROP COLUMN "sku"
-        `);
-    await queryRunner.query(`
             ALTER TABLE "products"
-            ADD "sku" character varying(30) NOT NULL
-        `);
-    await queryRunner.query(`
-            ALTER TABLE "products"
-            ADD CONSTRAINT "uq_products_sku" UNIQUE ("sku")
-        `);
-    await queryRunner.query(`
-            CREATE INDEX "idx_products_sku" ON "products" ("sku")
+            ALTER COLUMN "sku" TYPE character varying(30)
         `);
   }
 }
