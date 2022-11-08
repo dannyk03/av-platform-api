4cf8c7763830b039f644474fe96c3e33d5701197
liat <liatyosef18@gmail.com>
2022-11-08 23:28:27 +0200

    increased file size

--- src/config/debugger.config.ts
+++ src/config/debugger.config.ts
@@ -9,14 +9,14 @@ export default registerAs(
       writeIntoFile: process.env.DEBUGGER_HTTP_WRITE_INTO_FILE === 'true',
       writeIntoConsole: process.env.DEBUGGER_HTTP_WRITE_INTO_CONSOLE === 'true',
       maxFiles: 5,
-      maxSize: '2M',
+      maxSize: '5M',
     },
     system: {
       writeIntoFile: process.env.DEBUGGER_SYSTEM_WRITE_INTO_FILE === 'true',
       writeIntoConsole:
         process.env.DEBUGGER_SYSTEM_WRITE_INTO_CONSOLE === 'true',
       maxFiles: ms('7d'),
-      maxSize: '2m',
+      maxSize: '5m',
     },
   }),
 );
