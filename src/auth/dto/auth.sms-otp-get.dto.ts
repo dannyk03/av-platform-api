be815278c6e844450341b85ebefa4b611ba08126
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-11-22 16:10:32 +0200

    Fix OTP verify

--- src/auth/dto/auth.sms-otp-get.dto.ts
+++ src/auth/dto/auth.sms-otp-get.dto.ts
@@ -4,13 +4,11 @@ import { Type } from 'class-transformer';
 import { IsPhoneNumber } from 'class-validator';
 
 import { NormalizeStringInputTransform } from '@/utils/request/transform';
-import { IsRegisteredUser } from '@/utils/request/validation';
 
 export class AuthSmsOtpGetDto {
   @IsPhoneNumber()
   @NormalizeStringInputTransform()
   @Type(() => String)
-  @IsRegisteredUser()
   @ApiProperty()
   readonly phoneNumber: string;
 }
