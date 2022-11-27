f3ba0639dafe0656c7653fec33a6674f58ea6701
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-11-27 16:43:23 +0200

    Fix group member role nullable

--- src/group/entity/group-member.entity.ts
+++ src/group/entity/group-member.entity.ts
@@ -25,7 +25,6 @@ export class GroupMember extends BaseEntity<GroupMember> {
     type: 'enum',
     enum: EnumGroupRole,
     default: EnumGroupRole.Basic,
-    nullable: true,
   })
   role!: EnumGroupRole;
 }
