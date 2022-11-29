8481c76ec9aad0110997f2ca04773aba9a6bb36d
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-11-30 00:29:40 +0200

    Cleanup

--- src/utils/helper/service/helper.email.service.ts
+++ src/utils/helper/service/helper.email.service.ts
@@ -25,10 +25,6 @@ export class HelperEmailService implements IHelperEmailService {
   }
 
   isAcceptableEmail(email: string): boolean {
-    const xxx = isEmail(email);
-    const xxx1 = isEmail(this.isDisposableEmailDomain(email));
-    const xxx2 = isEmail(this.isFreeEmailDomain(email));
-
     return (
       isEmail(email) &&
       !this.isDisposableEmailDomain(email) &&
