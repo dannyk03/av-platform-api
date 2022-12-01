1e3cb016564586413e0c85481d488ef7764d31ea
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-12-01 10:32:13 +0200

    Sonar fixes

--- src/cache/redis/redis-server/service/redis-server.service.ts
+++ src/cache/redis/redis-server/service/redis-server.service.ts
@@ -1,29 +1,6 @@
-import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-
-// import { RedisMemoryServer } from 'redis-memory-server';
-
-export class RedisServerService implements OnModuleInit, OnModuleDestroy {
+export class RedisServerService {
   private readonly server;
-  // private readonly server = new RedisMemoryServer({
-  //   autoStart: true,
-  //   instance: {
-  //     port: parseInt(process.env.REDIS_PORT),
-  //     ip: process.env.REDIS_HOST,
-  //   },
-  // });
-  async onModuleInit() {
-    //   console.log(
-    //     `Redis server running on ${await this.server.getHost()}:${await this.server.getPort()}`,
-    //   );
-  }
-  async onModuleDestroy() {
-    //   try {
-    //     await this.server.stop();
-    //     console.log(`The local Redis server is off`);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-  }
+
   async getPort(): Promise<number | undefined> {
     return this.server?.getPort();
   }
