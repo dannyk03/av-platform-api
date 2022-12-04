ae91ba2875ce634f7f7085e6eb82ef37d761b007
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-12-04 18:26:38 +0200

    Cleanup

--- src/group/serialization/group.upcoming-milestones.list.serialization.ts
+++ src/group/serialization/group.upcoming-milestones.list.serialization.ts
@@ -9,14 +9,10 @@ import dayjs from 'dayjs';
 import lowerCase from 'lodash/lowerCase';
 import upperFirst from 'lodash/upperFirst';
 
-import { HelperDateService } from '@/utils/helper/service';
-
 @Exclude()
 export class GroupUpcomingMilestonesListSerialization
   implements IGroupUpcomingMilestonesGetSerialization
 {
-  constructor(private readonly helperDateService: HelperDateService) {}
-
   @Expose()
   role: EnumGroupRole;
 
@@ -40,9 +36,7 @@ export class GroupUpcomingMilestonesListSerialization
   year: number;
 
   @Expose()
-  @Transform(({ obj }) =>
-    dayjs(`${obj.year}-${obj.month}-${obj.day}`).format('MMM Do'),
-  )
+  @Transform(({ obj }) => dayjs(obj.date).format('MMM Do'))
   dateFormat: string;
 
   @Expose()
