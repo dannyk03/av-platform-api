1f830976bf167599817d66f275c6d25ef1e67a15
Stephan Sarkisyan <sark.stephan@gmail.com>
2022-12-05 14:22:11 +0200

    WIP

--- src/jobs/producer/proactive-email.producer.ts
+++ src/jobs/producer/proactive-email.producer.ts
@@ -1,32 +1,26 @@
-import { InjectQueue } from '@nestjs/bullmq';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-
-import { Queue } from 'bullmq';
 
 import { UserProfileService } from '@/user/service';
 
 import { EnumJobsCronName } from '../constant';
-import { EnumJobsQueue } from '@/queue/constant';
+
+// import { EnumJobsQueue } from '@/queue/constant';
+// import { Queue } from 'bullmq';
+// import { InjectQueue } from '@nestjs/bullmq';
 
 @Injectable()
 export class ProactiveEmailProducer {
   constructor(
-    @InjectQueue(EnumJobsQueue.ProactiveEmail)
-    private readonly proactiveEmailQueue: Queue,
+    // @InjectQueue(EnumJobsQueue.ProactiveEmail)
+    // private readonly proactiveEmailQueue: Queue,
     private readonly userProfileService: UserProfileService,
   ) {}
 
-  @Cron(CronExpression.EVERY_HOUR, {
+  @Cron(CronExpression.EVERY_DAY_AT_1AM, {
     name: EnumJobsCronName.NextWeekBirthday,
   })
   async handleNextWeekBirthdayProactiveNotification() {
-    try {
-      await this.proactiveEmailQueue.add(EnumJobsCronName.NextWeekBirthday, {
-        foo: 'bar',
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    console.log('first');
   }
 }
--- src/jobs/service/proactive-email.service.ts
+++ src/jobs/service/proactive-email.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectDataSource } from '@nestjs/typeorm';
+
+import { DataSource } from 'typeorm';
 
 import { CustomerIOService } from '@/messaging/customer-io/service';
+import { UserService } from '@/user/service';
+
+import { ConnectionNames } from '@/database/constant';
 
 @Injectable()
 export class ProactiveEmailService {
-  private readonly isDevelopment: boolean =
-    this.configService.get<boolean>('app.isDevelopment');
+  private readonly isProduction: boolean =
+    this.configService.get<boolean>('app.isProduction');
 
   constructor(
+    @InjectDataSource(ConnectionNames.Default)
+    private readonly defaultDataSource: DataSource,
     private readonly configService: ConfigService,
-    private readonly customerIOService: CustomerIOService,
   ) {}
 
-  async sendBirthdayNotification(data) {
-    console.log({
-      data,
-      separator:
-        '############################################################################################################################################################################################################################',
-    });
+  async getBirthdayInXDaysEmailData(inDays: number) {
+    return this.defaultDataSource.query(
+      `
+
+    `,
+    );
   }
 }
