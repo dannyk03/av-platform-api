7bed7da83a7de48166c689f0dae38e3730a4aae1
Sergey Zubyk <zubyk.sergey@gmail.com>
2022-12-14 12:49:51 +0200

    remove unused

--- src/group/dto/group.add-member.dto.ts
+++ src/group/dto/group.add-member.dto.ts
@@ -3,13 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { EnumAddGroupMemberType } from '@avo/type';
 
 import { Type } from 'class-transformer';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Length,
-} from 'class-validator';
+import { IsEnum, IsOptional, IsString, Length } from 'class-validator';
 
 export class GroupInviteAcceptRefDto {
   @IsOptional()
