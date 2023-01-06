4e9340a7e3ec58bf25230d3a9c4ee317f7413be9
maks-zu <zubovmaksim@gmail.com>
2023-01-06 15:23:53 +0100

    A20-421 CreateGroupQuestion endpoint

--- src/group/controller/group-question.controller.ts
+++ src/group/controller/group-question.controller.ts
@@ -1,27 +1,36 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
-  Param,
+  Post,
   Query,
 } from '@nestjs/common';
 
-import { IResponsePagingData } from '@avo/type';
+import { IResponseData, IResponsePagingData } from '@avo/type';
 
+import { Group } from '@/group/entity';
 import { User } from '@/user/entity';
 
 import { GroupQuestionService } from '@/group/service';
 
-import { CanAccessAsGroupMember } from '@/group/decorator';
+import { CanAccessAsGroupMember, ReqGroup } from '@/group/decorator';
+import { LogTrace } from '@/log/decorator';
 import { ReqAuthUser } from '@/user/decorator';
-import { ClientResponsePaging } from '@/utils/response/decorator';
+import {
+  ClientResponse,
+  ClientResponsePaging,
+} from '@/utils/response/decorator';
 
 import { AclGuard } from '@/auth/guard';
 
 import { GroupQuestionListDto } from '@/group/dto';
+import { GroupQuestionCreateDto } from '@/group/dto/group-question.create.dto';
 
 import { GroupQuestionGetSerialization } from '@/group/serialization';
+
+import { EnumLogAction } from '@/log/constant';
 
 @Controller({
   version: '1',
@@ -41,9 +50,30 @@ export class GroupQuestionController {
     user: User,
     @Query()
     dto: GroupQuestionListDto,
-    @Param('groupId')
-    groupId: string,
+    @ReqGroup()
+    group: Group,
   ): Promise<IResponsePagingData> {
-    return this.groupQuestionService.getGroupPaginatedList(user, dto, groupId);
+    return this.groupQuestionService.getGroupPaginatedList(user, dto, group);
+  }
+
+  @ClientResponse('group.question.create', {
+    classSerialization: GroupQuestionGetSerialization,
+  })
+  @LogTrace(EnumLogAction.CreateGroupQuestion, {
+    tags: ['group', 'question', 'create'],
+  })
+  @HttpCode(HttpStatus.OK)
+  @CanAccessAsGroupMember()
+  @AclGuard()
+  @Post()
+  createQuestion(
+    @ReqAuthUser()
+    user: User,
+    @ReqGroup()
+    group: Group,
+    @Body()
+    dto: GroupQuestionCreateDto,
+  ): Promise<IResponseData> {
+    return this.groupQuestionService.createGroupQuestion(user, dto, group);
   }
 }
