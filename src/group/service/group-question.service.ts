611c524a46f37c107de502491e0c4b9d98dc9e02
Stephan Sarkisyan <sark.stephan@gmail.com>
2023-01-13 13:37:53 +0200

    format

--- src/group/service/group-question.service.ts
+++ src/group/service/group-question.service.ts
@@ -90,7 +90,7 @@ export class GroupQuestionService {
       groupQuestion,
     );
     this.groupQuestionEmailProducer?.groupCreatedEmail(savedGroupQuestion);
-    
+
     return savedGroupQuestion;
   }
 
